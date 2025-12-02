ac8c5a5ed01014f60a8fdbd4221d832b71a729a4
Mallangc <cky9001@gmail.com>
2025-12-02 18:54:56 +0900

    feat/user - ci.yml 수정 - exception.code.ts의 삭제했던 코드 복구

--- src/exception/exception.code.ts
+++ src/exception/exception.code.ts
@@ -4,6 +4,7 @@ export const ExceptionCode = {
   ALREADY_EXISTS_USER: {message: '이미 존재하는 회원입니다.', status: 409},
   ALREADY_EXISTS_NICKNAME: {message: '이미 존재하는 닉네임입니다.', status: 409},
   WRONG_EMAIL_OR_PASSWORD: {message: '아이디나 비밀번호를 다시 확인해주세요.', status: 401},
+  TOKEN_INVALID: {message: '유효하지 않은 토큰입니다.', status: 401},
 
 
 
