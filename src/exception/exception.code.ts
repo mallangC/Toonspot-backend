f4e6ce0c4684efac6412b4506a92cf2bc4329cd9
Mallangc <cky9001@gmail.com>
2026-01-07 08:51:28 +0900

    feat/toon - exception 코드 추가 및 적용 - UNAUTHORIZED 추가 - role.guard.ts, toon.controller.spec.ts에 사용된 예와처리에 적용

--- src/exception/exception.code.ts
+++ src/exception/exception.code.ts
@@ -5,9 +5,14 @@ export const ExceptionCode = {
   NICKNAME_ALREADY_EXISTS: {message: '이미 존재하는 닉네임입니다.', status: 409},
   CREDENTIALS_INVALID: {message: '아이디나 비밀번호를 확인해주세요.', status: 401},
   TOKEN_INVALID: {message: '유효하지 않은 토큰입니다.', status: 401},
+  UNAUTHORIZED: {message: '접근 권한이 없습니다.', status: 403},
 
   // 만화
   TOON_ALREADY_EXISTS: {message: '이미 존재하는 만화입니다.', status: 400},
   TOON_NOT_FOUND: {message: '만화를 찾을 수 없습니다.', status: 400},
+
+  // 게시물
+  POST_NOT_FOUND: {message: '게시물을 찾을 수 없습니다.', status: 400},
+  POST_NOT_OWNER: {message: '해당 게시글에 대한 권한이 없습니다.', status: 403},
 
 } as const;
