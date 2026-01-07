6be5f27f5a39d295893552d7701e5d8a2786c13b
Mallangc <cky9001@gmail.com>
2026-01-07 09:31:40 +0900

    feat/post - post 로직에서 사용될 user repository 함수 추가 - roles 데코레이터 리팩토링

--- src/decorators/user.roles.decorator.ts
+++ src/decorators/user.roles.decorator.ts
@@ -1,8 +1,5 @@
-// roles.decorator.ts
-
 import { SetMetadata } from '@nestjs/common';
 import {Role} from "../type/user.type";
 
 export const ROLES_KEY = 'roles';
-// 필요한 역할을 배열로 받아 메타데이터로 설정합니다.
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
