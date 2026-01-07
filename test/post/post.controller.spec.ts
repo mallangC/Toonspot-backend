6626dbd978f6e53a836e275091162d3e36206c6c
Mallangc <cky9001@gmail.com>
2026-01-07 09:39:24 +0900

    feat/post - post 테스트 코드 작성 - 최소 한번의 코드는 실행 되도록 코드 작성

---
+++ test/post/post.controller.spec.ts
@@ -0,0 +1,356 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {INestApplication, ValidationPipe} from "@nestjs/common";
+import {AppModule} from "../../src/app.module";
+import {JwtAuthGuard} from "../../src/auth/jwt/jwt.guard";
+import {MockAuthGuard} from "../mocks/mock-auth.guard";
+import {PrismaService} from "../../prisma/prisma.service";
+import {Role} from "../../src/type/user.type";
+import request from "supertest";
+import {PostCreateDto} from "../../src/post/dto/post.create.dto";
+import {PostStatus} from "@prisma/client";
+import {ExceptionCode} from "../../src/exception/exception.code";
+import {PostUpdateDto} from "../../src/post/dto/post.update.dto";
+import {PostUpdateStatusDto} from "../../src/post/dto/post.update.status.dto";
+
+describe('PostController', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let testUser: any;
+  let adminUser: any;
+
+  const createDto = {
+    title: '테스트 게시글',
+    content: '테스트 게시글 내용',
+  } as PostCreateDto
+
+  const updateDto = {
+    title: '수정된 게시글 제목',
+    content: '수정된 게시글 내용',
+  } as PostUpdateDto;
+
+  const updateStatusDto = {
+    status: PostStatus.HIDDEN
+  } as PostUpdateStatusDto
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).overrideGuard(JwtAuthGuard)
+        .useValue(new MockAuthGuard())
+        .compile();
+    app = module.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
+    }));
+    await app.init();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    const userData = {
+      id: 1,
+      email: 'test@email.com',
+      password: 'password1234',
+      nickname: '김테스트',
+      role: Role.USER
+    }
+    const adminData = {
+      id: 2,
+      email: 'admin@email.com',
+      password: 'password1234',
+      nickname: '김어드민',
+      role: Role.ADMIN
+    }
+    testUser = await prisma.user.create({data: userData});
+    adminUser = await prisma.user.create({data: adminData});
+  });
+
+  beforeEach(async () => {
+    await prisma.post.deleteMany();
+    MockAuthGuard.mockUser = {
+      id: testUser.id,
+      email: testUser.email,
+      nickname: testUser.nickname,
+      role: Role.USER
+    };
+  });
+
+  afterAll(async () => {
+    await prisma.post.deleteMany();
+    await prisma.toon.deleteMany();
+    await prisma.user.deleteMany();
+    await app.close();
+  });
+
+  it('POST /post : 게시글 등록 성공', () => {
+    return request(app.getHttpServer())
+        .post('/post')
+        .send(createDto)
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.title).toEqual(createDto.title);
+          expect(res.body.data.content).toEqual(createDto.content);
+          expect(res.body.data.userId).toEqual(testUser.id);
+          expect(res.body.data.status).toEqual(PostStatus.PUBLISHED);
+        })
+  });
+
+  it('GET /post?page=1 : 게시글 페이징 조회 성공', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: testUser.id, status: PostStatus.HIDDEN}});
+
+    return request(app.getHttpServer())
+        .get('/post?page=1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.items.length).toEqual(1);
+          expect(res.body.data.items[0].title).toEqual(createDto.title);
+          expect(res.body.data.items[0].content).toEqual(createDto.content);
+          expect(res.body.data.items[0].userId).toEqual(testUser.id);
+          expect(res.body.data.items[0].status).toEqual(PostStatus.PUBLISHED);
+        })
+  });
+
+  it('GET /post/admin?page=1 : (관리자) 게시글 페이징 조회 성공', async () => {
+    MockAuthGuard.mockUser = {
+      id: adminUser.id,
+      email: adminUser.email,
+      nickname: adminUser.nickname,
+      role: Role.ADMIN
+    };
+    await prisma.post.create({data: {...createDto, userId: adminUser.id}});
+    await prisma.post.create({data: {...createDto, userId: adminUser.id, status: PostStatus.HIDDEN}});
+
+    return request(app.getHttpServer())
+        .get('/post/admin?page=1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.items.length).toEqual(2);
+          expect(res.body.data.items[0].title).toEqual(createDto.title);
+          expect(res.body.data.items[0].content).toEqual(createDto.content);
+          expect(res.body.data.items[0].userId).toEqual(adminUser.id);
+          expect(res.body.data.items[0].status).toEqual(PostStatus.HIDDEN);
+        })
+  });
+
+  it('GET /post/admin?page=1 : (관리자) 게시글 페이징 조회 실패 (관리자가 아님)', async () => {
+    await prisma.post.create({data: {...createDto, userId: adminUser.id}});
+    await prisma.post.create({data: {...createDto, userId: adminUser.id, status: PostStatus.HIDDEN}});
+
+    return request(app.getHttpServer())
+        .get('/post/admin?page=1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.message).toEqual(ExceptionCode.UNAUTHORIZED.message);
+        })
+  });
+
+  it('GET /post/me : 내 게시글 조회 성공', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: adminUser.id}});
+
+    return request(app.getHttpServer())
+        .get('/post/me?page=1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.items.length).toEqual(2);
+          expect(res.body.data.items[0].title).toEqual(createDto.title);
+          expect(res.body.data.items[0].content).toEqual(createDto.content);
+          expect(res.body.data.items[0].userId).toEqual(testUser.id);
+          expect(res.body.data.items[0].status).toEqual(PostStatus.PUBLISHED);
+        })
+  });
+
+  it('GET /post/me : 해당 유저 게시글 조회 성공 (관리자)', async () => {
+    MockAuthGuard.mockUser = {
+      id: adminUser.id,
+      email: adminUser.email,
+      nickname: adminUser.nickname,
+      role: Role.ADMIN
+    };
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: adminUser.id}});
+
+    return request(app.getHttpServer())
+        .get('/post/admin/me?userId=1&page=1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.items.length).toEqual(2);
+          expect(res.body.data.items[0].title).toEqual(createDto.title);
+          expect(res.body.data.items[0].content).toEqual(createDto.content);
+          expect(res.body.data.items[0].userId).toEqual(testUser.id);
+          expect(res.body.data.items[0].status).toEqual(PostStatus.PUBLISHED);
+        })
+  });
+
+  it('GET /post/me : (관리자) 해당 유저 게시글 조회 실패 (해당 유저를 찾을 수 없음)', async () => {
+    MockAuthGuard.mockUser = {
+      id: adminUser.id,
+      email: adminUser.email,
+      nickname: adminUser.nickname,
+      role: Role.ADMIN
+    };
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: testUser.id}});
+    await prisma.post.create({data: {...createDto, userId: adminUser.id}});
+
+    return request(app.getHttpServer())
+        .get('/post/admin/me?userId=3&page=1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.message).toEqual(ExceptionCode.USER_NOT_FOUND.message);
+        })
+  });
+
+  it('GET /post/:id : 단일 게시물 조회 성공', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    return request(app.getHttpServer())
+        .get('/post/1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.title).toEqual(createDto.title);
+          expect(res.body.data.content).toEqual(createDto.content);
+          expect(res.body.data.userId).toEqual(testUser.id);
+          expect(res.body.data.status).toEqual(PostStatus.PUBLISHED);
+        })
+  });
+
+  it('GET /post/:id : 단일 게시물 조회 실패 (일반 유저 > 상태 HIDDEN, DELETE 조회 불가)', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1, status: PostStatus.HIDDEN}});
+
+    return request(app.getHttpServer())
+        .get('/post/1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.message).toEqual(ExceptionCode.POST_NOT_FOUND.message);
+        })
+  });
+
+  it('GET /post/admin/:id : (관리자) 단일 게시물 조회 성공 (관리자 > 상태 HIDDEN, DELETE 조회 가능)', async () => {
+    MockAuthGuard.mockUser = {
+      id: adminUser.id,
+      email: adminUser.email,
+      nickname: adminUser.nickname,
+      role: Role.ADMIN
+    };
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1, status: PostStatus.HIDDEN}});
+
+    return request(app.getHttpServer())
+        .get('/post/admin/1')
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.title).toEqual(createDto.title);
+          expect(res.body.data.content).toEqual(createDto.content);
+          expect(res.body.data.userId).toEqual(testUser.id);
+          expect(res.body.data.status).toEqual(PostStatus.HIDDEN);
+        })
+  });
+
+  it('PATCH /post/:id : 게시물 수정 성공', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    return request(app.getHttpServer())
+        .patch('/post/1')
+        .send(updateDto)
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.title).toEqual(updateDto.title);
+          expect(res.body.data.content).toEqual(updateDto.content);
+          expect(res.body.data.userId).toEqual(testUser.id);
+          expect(res.body.data.status).toEqual(PostStatus.PUBLISHED);
+        })
+  });
+
+  it('PATCH /post/:id : 게시물 수정 실패 (자신의 게시물이 아님)', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+    await prisma.post.create({data: {...createDto, userId: adminUser.id, id: 2}});
+
+    return request(app.getHttpServer())
+        .patch('/post/2')
+        .send(updateDto)
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.message).toEqual(ExceptionCode.POST_NOT_OWNER.message);
+        })
+  });
+
+  it('PATCH /post/status/:id : 게시물 수정 성공', async () => {
+    MockAuthGuard.mockUser = {
+      id: adminUser.id,
+      email: adminUser.email,
+      nickname: adminUser.nickname,
+      role: Role.ADMIN
+    };
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    return request(app.getHttpServer())
+        .patch('/post/status/1')
+        .send(updateStatusDto)
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.data.title).toEqual(createDto.title);
+          expect(res.body.data.content).toEqual(createDto.content);
+          expect(res.body.data.userId).toEqual(testUser.id);
+          expect(res.body.data.status).toEqual(updateStatusDto.status);
+        })
+  });
+
+  it('PATCH /post/status/:id : 게시물 수정 실패 (일반 유저 > 접근 불가)', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    return request(app.getHttpServer())
+        .patch('/post/status/1')
+        .send(updateStatusDto)
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.message).toEqual(ExceptionCode.UNAUTHORIZED.message);
+        })
+  });
+
+  it('PATCH /post/status/:id : 게시물 수정 실패 (게시물을 찾을 수 없음)', async () => {
+    MockAuthGuard.mockUser = {
+      id: adminUser.id,
+      email: adminUser.email,
+      nickname: adminUser.nickname,
+      role: Role.ADMIN
+    };
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    return request(app.getHttpServer())
+        .patch('/post/status/2')
+        .send(updateStatusDto)
+        .expect(res => {
+          console.log(JSON.stringify(res.body, null, 2));
+          expect(res.body.message).toEqual(ExceptionCode.POST_NOT_FOUND.message);
+        })
+  });
+
+
+  it('DELETE /post/:id : 게시물 삭제 처리 성공 (soft delete)', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    const result = await request(app.getHttpServer())
+        .delete('/post/1')
+        .send(updateStatusDto);
+    expect(result.body.data).toEqual('1번 게시물이 삭제되었습니다.');
+
+    const findPost = await prisma.post.findUnique({where: {id: 1}});
+    expect(findPost!.status).toEqual(PostStatus.DELETED);
+  });
+
+  it('DELETE /post/:id : 게시물 삭제 처리 실패 (게시물을 찾을 수 없음)', async () => {
+    await prisma.post.create({data: {...createDto, userId: testUser.id, id: 1}});
+
+    return request(app.getHttpServer())
+        .delete('/post/2')
+        .send(updateStatusDto)
+        .expect(res => {
+          expect(res.body.message).toEqual(ExceptionCode.POST_NOT_FOUND.message);
+        });
+  });
+});
---
+++ test/post/post.service.spec.ts
@@ -0,0 +1,206 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {PostService} from '../../src/post/post.service';
+import {PostRepository} from "../../src/post/post.repository";
+import {UserRepository} from "../../src/user/user.repository";
+import {PostCreateDto} from "../../src/post/dto/post.create.dto";
+import {PostResponse} from "../../src/post/dto/post.response";
+import {PostStatus} from "@prisma/client";
+import {ExceptionCode} from "../../src/exception/exception.code";
+import {PostGetPagingDto} from "../../src/post/dto/post.get.paging.dto";
+import {PostPagingResponse} from "../../src/post/dto/post.paging.response";
+import {PostUpdateDto} from "../../src/post/dto/post.update.dto";
+import {PostUpdateStatusDto} from "../../src/post/dto/post.update.status.dto";
+
+describe('PostService', () => {
+  let postService: PostService;
+  let postRepository: PostRepository;
+  let userRepository: UserRepository;
+  const mockPostRepository = {
+    save: jest.fn(),
+    findById: jest.fn(),
+    findAll: jest.fn(),
+    findAllByUserId: jest.fn(),
+    update: jest.fn(),
+    updateStatus: jest.fn(),
+    delete: jest.fn(),
+    existsById: jest.fn(),
+  }
+
+  const mockUserRepository = {
+    existsById: jest.fn()
+  }
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PostService, {
+        provide: PostRepository,
+        useValue: mockPostRepository,
+      }, {
+        provide: UserRepository,
+        useValue: mockUserRepository,
+      }],
+    }).compile();
+
+    postService = module.get<PostService>(PostService);
+    postRepository = module.get<PostRepository>(PostRepository);
+    userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  const dto = {
+    title: '테스트 제목',
+    content: '테스트 게시물 내용',
+  } as PostCreateDto
+
+  const postResponse = {
+    id: 1,
+    userId: 1,
+    title: "테스트 제목",
+    content: "테스트 게시물 내용",
+    status: PostStatus.PUBLISHED,
+    viewCount: 0,
+    likeCount: 0,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  } as PostResponse
+
+  const pagingDto = {
+    page: 1
+  } as PostGetPagingDto
+
+  const pagingResponse = {
+    items: [postResponse],
+    metadata: {
+      totalCount: 1,
+      totalPages: 1,
+      currentPage: 1
+    }
+  } as PostPagingResponse
+
+  it('should be defined', () => {
+    expect(postService).toBeDefined();
+    expect(postRepository).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  it('게시물 추가 성공', async () => {
+    (postRepository.save as jest.Mock).mockResolvedValue(postResponse);
+
+    const result = await postService.createPost(dto, postResponse.userId);
+    console.log(JSON.stringify(result, null, 2))
+    expect(postRepository.save).toHaveBeenCalledWith(dto, postResponse.userId);
+    expect(result?.id).toEqual(postResponse.id);
+  });
+
+  it('게시물 단일 조회 성공', async () => {
+    (postRepository.findById as jest.Mock).mockResolvedValue(postResponse);
+
+    const result = await postService.getPost(postResponse.id, true);
+    console.log(JSON.stringify(result, null, 2))
+    expect(postRepository.findById).toHaveBeenCalledWith(postResponse.id, true);
+    expect(result?.id).toEqual(postResponse.id);
+  });
+
+  it('게시물 단일 조회 실패 (게시물을 찾을 수 없음)', async () => {
+    (postRepository.findById as jest.Mock).mockResolvedValue(null);
+
+    await expect(postService.getPost(postResponse.id, true)).rejects.toThrow(ExceptionCode.POST_NOT_FOUND.message);
+    expect(postRepository.findById).toHaveBeenCalledWith(postResponse.id, true);
+  });
+
+  it('게시물 페이징 조회 성공', async () => {
+
+    (postRepository.findAll as jest.Mock).mockResolvedValue(pagingResponse);
+
+    const result = await postService.getPostsPaged(pagingDto, true);
+    console.log(JSON.stringify(result, null, 2))
+    expect(postRepository.findAll).toHaveBeenCalledWith(pagingDto, true);
+    expect(result?.items[0].id).toEqual(postResponse.id);
+  });
+
+  it('내 게시물 페이징 조회 성공', async () => {
+    (postRepository.findAllByUserId as jest.Mock).mockResolvedValue(pagingResponse);
+    (userRepository.existsById as jest.Mock).mockResolvedValue(true);
+
+    const result = await postService.getUserPosts(pagingDto, postResponse.userId, true);
+    console.log(JSON.stringify(result, null, 2))
+    expect(userRepository.existsById).toHaveBeenCalledWith(postResponse.userId);
+    expect(postRepository.findAllByUserId).toHaveBeenCalledWith(pagingDto, postResponse.userId, true);
+    expect(result?.items[0].id).toEqual(postResponse.id);
+  });
+
+  it('내 게시물 페이징 조회 실패 (해당 유저를 찾을 수 없음)', async () => {
+    (userRepository.existsById as jest.Mock).mockResolvedValue(false);
+
+    await expect(postService.getUserPosts(pagingDto, postResponse.userId, true)).rejects.toThrow(ExceptionCode.USER_NOT_FOUND.message);
+    expect(userRepository.existsById).toHaveBeenCalledWith(postResponse.userId);
+    expect(postRepository.findAllByUserId).not.toHaveBeenCalledWith(pagingDto, postResponse.userId, true);
+  });
+
+  it('게시물 업데이트 성공', async () => {
+    const dto = {
+      title: '업데이트 제목',
+      content: '업데이트 게시물 내용',
+    } as PostUpdateDto
+
+    (postRepository.findById as jest.Mock).mockResolvedValue(postResponse);
+    (postRepository.update as jest.Mock).mockResolvedValue({...postResponse, ...dto});
+
+    const result = await postService.updatePost(postResponse.id, dto, postResponse.userId);
+    console.log(JSON.stringify(result, null, 2))
+    expect(postRepository.findById).toHaveBeenCalledWith(postResponse.userId, false);
+    expect(postRepository.update).toHaveBeenCalledWith(postResponse.id, dto);
+    expect(result?.title).toEqual(dto.title);
+    expect(result?.content).toEqual(dto.content);
+  });
+
+  it('게시물 업데이트 실패 (해당 userId가 내 userId와 맞지 않음)', async () => {
+    const dto = {
+      title: '업데이트 제목',
+      content: '업데이트 게시물 내용',
+    } as PostUpdateDto
+
+    (postRepository.findById as jest.Mock).mockResolvedValue({...postResponse, userId: 2});
+
+    await expect(postService.updatePost(postResponse.id, dto, postResponse.userId)).rejects.toThrow(ExceptionCode.POST_NOT_OWNER.message);
+    expect(postRepository.findById).toHaveBeenCalledWith(postResponse.userId, false);
+  });
+
+  it('게시물 상태 업데이트 성공', async () => {
+    const dto = {
+      status: PostStatus.HIDDEN
+    } as PostUpdateStatusDto
+
+    (postRepository.existsById as jest.Mock).mockResolvedValue({id: postResponse.id});
+    (postRepository.updateStatus as jest.Mock).mockResolvedValue({...postResponse, ...dto});
+
+    const result = await postService.updateStatus(postResponse.id, dto.status);
+    console.log(JSON.stringify(result, null, 2))
+    expect(postRepository.existsById).toHaveBeenCalledWith(postResponse.id);
+    expect(postRepository.updateStatus).toHaveBeenCalledWith(postResponse.id, dto.status);
+    expect(result?.status).toEqual(dto.status);
+  });
+
+  it('게시물 상태 업데이트 실패 (해당 게시물을 찾을 수 없음)', async () => {
+    const dto = {
+      status: PostStatus.HIDDEN
+    } as PostUpdateStatusDto
+
+    (postRepository.existsById as jest.Mock).mockResolvedValue(null);
+
+    await expect(postService.updateStatus(postResponse.id, dto.status)).rejects.toThrow(ExceptionCode.POST_NOT_FOUND.message);
+    expect(postRepository.existsById).toHaveBeenCalledWith(postResponse.id);
+  });
+
+  it('게시물 삭제 처리 성공', async () => {
+
+    (postRepository.findById as jest.Mock).mockResolvedValue(postResponse);
+    (postRepository.delete as jest.Mock).mockResolvedValue(`${postResponse.id}번 게시물이 삭제되었습니다.`);
+
+    const result = await postService.deletePost(postResponse.id, postResponse.userId);
+    console.log(JSON.stringify(result, null, 2))
+    expect(postRepository.findById).toHaveBeenCalledWith(postResponse.id, false);
+    expect(postRepository.delete).toHaveBeenCalledWith(postResponse.id);
+    expect(result).toEqual(`${postResponse.id}번 게시물이 삭제되었습니다.`);
+  });
+});
