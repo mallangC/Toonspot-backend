6ec50342ef51465688020c360453c4365d5d12a7
Mallangc <cky9001@gmail.com>
2026-01-07 09:38:35 +0900

    feat/post - post controller, service, repository 추가 - 등록, 전체 조회 (유저, 관리자), 단일 조회 (유저, 관리자), 내 게시물 전체 조회(유저, 관리자), 수정, 상태 수정(관리자), 삭제 (soft delete) 기능 구현 - 관리자가 게시물의 상태를 HIDDEN으로 처리했거나 유저가 자신의 게시물을 삭제하여 상태가 DELETE가 되었을 경우 일반 유저는 확인 할 수 없고 관리자 권한으로만 확인 가능

---
+++ src/post/post.controller.ts
@@ -0,0 +1,82 @@
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards} from '@nestjs/common';
+import {PostService} from "./post.service";
+import {JwtAuthGuard} from "../auth/jwt/jwt.guard";
+import {RoleGuard} from "../auth/role.guard";
+import {PostCreateDto} from "./dto/post.create.dto";
+import {UserResponseDto} from "../user/dto/user.response.dto";
+import {CurrentUser} from "../decorators/user.decorator";
+import {PostResponse} from "./dto/post.response";
+import {Roles} from "../decorators/user.roles.decorator";
+import {Role} from "../type/user.type";
+import {PostGetPagingDto} from "./dto/post.get.paging.dto";
+import {PostUpdateStatusDto} from "./dto/post.update.status.dto";
+import {PostUpdateDto} from "./dto/post.update.dto";
+import {PostGetPagingAdminDto} from "./dto/post.get.paging.admin.dto";
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  createPost(@Body() body: PostCreateDto, @CurrentUser() user: UserResponseDto): Promise<PostResponse>{
+    return this.postService.createPost(body, user.id);
+  }
+
+  @Get()
+  getPostsPaged(@Query() dto: PostGetPagingDto) {
+    return this.postService.getPostsPaged(dto, false);
+  }
+
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
+  @Get('admin')
+  getPostsPagedForAdmin(@Query() dto: PostGetPagingDto) {
+    return this.postService.getPostsPaged(dto, true);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getUserPosts(@Query() dto: PostGetPagingDto, @CurrentUser() user: UserResponseDto) {
+    return this.postService.getUserPosts(dto, user.id, false);
+  }
+
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
+  @Get('admin/me')
+  getUserPostsForAdmin(@Query() dto: PostGetPagingAdminDto) {
+    return this.postService.getUserPosts(dto, dto.userId, true);
+  }
+
+  @Get(':id')
+  getPost(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.getPost(id, false)
+  }
+
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
+  @Get('admin/:id')
+  getPostForAdmin(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.getPost(id, true)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  updatePost(@Param('id', ParseIntPipe) id: number, @Body() body: PostUpdateDto, @CurrentUser() user: UserResponseDto) {
+    return this.postService.updatePost(id, body, user.id);
+  }
+
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Roles(Role.ADMIN)
+  @Patch('status/:id')
+  updatePostStatus(@Param('id', ParseIntPipe) id: number, @Body() body: PostUpdateStatusDto) {
+    return this.postService.updateStatus(id, body.status);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  deletePost(@Param('id', ParseIntPipe) id: number, @CurrentUser() user: UserResponseDto) {
+    return this.postService.deletePost(id, user.id);
+  }
+}
