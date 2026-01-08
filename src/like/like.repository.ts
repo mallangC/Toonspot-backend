517446c27d0c24aa35eb96314ac2b5827a0fef33
Mallangc <cky9001@gmail.com>
2026-01-08 11:06:32 +0900

    feat/like - like repository 추가 - 이미 좋아요 버튼을 눌렀는지 안눌렀는지 확인하는 함수, 좋아요 눌렀을 때 실행할 save 함수, 좋아요를 한번 더 눌렀을 때 실행할 delete 함수까지 3가지 함수 구현

---
+++ src/like/like.repository.ts
@@ -0,0 +1,22 @@
+import {PrismaService} from "../../prisma/prisma.service";
+import {Injectable} from "@nestjs/common";
+import {POST_LIKE_SELECT} from "../../prisma/prisma.selects";
+
+@Injectable()
+export class LikeRepository {
+  constructor(private readonly prisma: PrismaService) {
+  }
+
+  async existsPostLike(userId: number, postId: number) {
+    const existsPostLike = await this.prisma.client.postLike.findUnique({where: {userId_postId: {userId, postId}}});
+    return !!existsPostLike;
+  }
+
+  async savePostLike(userId: number, postId: number) {
+    return this.prisma.client.postLike.create({data: {userId, postId}, select: POST_LIKE_SELECT});
+  }
+
+  async deletePostLike(userId: number, postId: number) {
+    await this.prisma.client.postLike.delete({where: {userId_postId: {userId, postId}}})
+  }
+}
