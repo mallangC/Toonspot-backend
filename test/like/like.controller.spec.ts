319827344995894a71b5ea9b0153b04ad40269e3
Mallangc <cky9001@gmail.com>
2026-01-12 00:29:29 +0900

    feat/comment - comment like 관련 테스트 코드 작성 - gitignore 에 *.sql 추가

--- test/like/like.controller.spec.ts
+++ test/like/like.controller.spec.ts
@@ -7,12 +7,14 @@ import {MockAuthGuard} from "../mocks/mock-auth.guard";
 import {Role} from "../../src/type/user.type";
 import {PostCreateDto} from "../../src/post/dto/post.create.dto";
 import request from "supertest";
+import {CommentDto} from "../../src/comment/dto/comment.dto";
 
 describe('LikeController', () => {
   let app: INestApplication;
   let prisma: PrismaService;
   let testUser: any;
   let basePost: any;
+  let baseComment: any;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -44,8 +46,13 @@ describe('LikeController', () => {
       content: '테스트 게시글 내용',
     } as PostCreateDto
 
+    const createCommentDto = {
+      content: '테스트 댓글 내용',
+    } as CommentDto
+
     testUser = await prisma.user.create({data: userData});
     basePost = await prisma.post.create({data: {...createPostDto, id: 1, userId: testUser.id}});
+    baseComment = await prisma.comment.create({data: {...createCommentDto, id: 1, userId: testUser.id, postId: basePost.id}});
     MockAuthGuard.mockUser = {
       id: testUser.id,
       email: testUser.email,
@@ -59,29 +66,30 @@ describe('LikeController', () => {
   });
 
   afterAll(async () => {
+    await prisma.commentLike.deleteMany();
+    await prisma.comment.deleteMany();
     await prisma.postLike.deleteMany();
     await prisma.post.deleteMany();
     await prisma.user.deleteMany();
     await app.close();
   });
 
-  it('PATCH /like/:postId : 좋아요 성공', async () => {
+  it('POST /like/post/:postId : 좋아요 성공', async () => {
     const result = await request(app.getHttpServer())
-        .post(`/like/${basePost.id}`);
+        .post(`/like/post/${basePost.id}`);
     console.log(JSON.stringify(result.body.data, null, 2))
     expect(result.body.data.liked).toEqual(true);
 
     const findPost = await prisma.post.findUnique({where: {id: basePost.id}});
     console.log(JSON.stringify(findPost, null, 2))
     expect(findPost!.likeCount).toEqual(1);
-
   });
 
-  it('PATCH /like/:postId : 좋아요 취소 성공', async () => {
+  it('POST /like/post/:postId : 좋아요 취소 성공', async () => {
     await prisma.post.update({where: {id: basePost.id}, data: {likeCount: 1}});
     await prisma.postLike.create({data: {postId: basePost.id, userId: testUser.id}});
     const result = await request(app.getHttpServer())
-        .post(`/like/${basePost.id}`);
+        .post(`/like/post/${basePost.id}`);
 
     console.log(JSON.stringify(result.body, null, 2));
     expect(result.body.data.liked).toEqual(false);
@@ -91,14 +99,25 @@ describe('LikeController', () => {
     expect(findPost!.likeCount).toEqual(0);
   });
 
-  it('PATCH /like/:postId : 좋아요 실패', async () => {
+  it('POST /like/:postId : 좋아요 실패', async () => {
     await prisma.postLike.create({data: {postId: basePost.id, userId: testUser.id}});
     return request(app.getHttpServer())
-        .post(`/like/2`)
+        .post(`/like/post/2`)
         .expect(res => {
           console.log(JSON.stringify(res.body, null, 2));
           expect(res.body.message).toEqual(`게시물을 찾을 수 없습니다.`);
         })
+  });
+
+  it('POST /like/comment/:commentId : 좋아요 성공', async () => {
+    const result = await request(app.getHttpServer())
+        .post(`/like/comment/${baseComment.id}`);
+    console.log(JSON.stringify(result.body.data, null, 2))
+    expect(result.body.data.liked).toEqual(true);
+
+    const findComment = await prisma.comment.findUnique({where: {id: baseComment.id}});
+    console.log(JSON.stringify(findComment, null, 2))
+    expect(findComment!.likeCount).toEqual(1);
   });
 
 });
--- test/like/like.service.spec.ts
+++ test/like/like.service.spec.ts
@@ -3,18 +3,28 @@ import {LikeService} from '../../src/like/like.service';
 import {LikeRepository} from "../../src/like/like.repository";
 import {PostRepository} from "../../src/post/post.repository";
 import {ExceptionCode} from "../../src/exception/exception.code";
+import {CommentRepository} from "../../src/comment/comment.repository";
 
 describe('LikeService', () => {
   let likeService: LikeService;
   let likeRepository: LikeRepository;
   let postRepository: PostRepository;
+  let commentRepository: CommentRepository;
   const mockLikeRepository = {
     existsPostLike: jest.fn(),
     savePostLike: jest.fn(),
     deletePostLike: jest.fn(),
+    existsCommentLike: jest.fn(),
+    saveCommentLike: jest.fn(),
+    deleteCommentLike: jest.fn(),
   }
 
   const mockPostRepository = {
+    existsById: jest.fn(),
+    updateLikeCount: jest.fn(),
+  }
+
+  const mockCommentRepository = {
     existsById: jest.fn(),
     updateLikeCount: jest.fn(),
   }
@@ -23,6 +33,7 @@ describe('LikeService', () => {
     id: 1,
     postId: 1,
     userId: 1,
+    commentId:1,
     createdAt: new Date()
   }
 
@@ -34,18 +45,23 @@ describe('LikeService', () => {
       },{
         provide: PostRepository,
         useValue: mockPostRepository,
+      },{
+        provide: CommentRepository,
+        useValue: mockCommentRepository,
       }],
     }).compile();
 
     likeService = module.get<LikeService>(LikeService);
     likeRepository = module.get<LikeRepository>(LikeRepository);
     postRepository = module.get<PostRepository>(PostRepository);
+    commentRepository = module.get<CommentRepository>(CommentRepository);
   });
 
   it('should be defined', () => {
     expect(likeService).toBeDefined();
     expect(likeRepository).toBeDefined();
     expect(postRepository).toBeDefined();
+    expect(commentRepository).toBeDefined();
   });
 
   it('게시물 좋아요 1번 토글 성공', async () => {
@@ -81,5 +97,40 @@ describe('LikeService', () => {
 
     await expect(likeService.togglePostLike(response.userId, response.postId)).rejects.toThrow(ExceptionCode.POST_NOT_FOUND.message);
     expect(postRepository.existsById).toHaveBeenCalledWith(response.postId);
+  });
+
+  it('댓글 좋아요 1번 토글 성공', async () => {
+    (commentRepository.existsById as jest.Mock).mockResolvedValue(true);
+    (likeRepository.existsCommentLike as jest.Mock).mockResolvedValue(false);
+    (likeRepository.saveCommentLike as jest.Mock).mockResolvedValue(response);
+
+    const result = await likeService.toggleCommentLike(response.userId, response.commentId);
+    console.log(JSON.stringify(result, null, 2));
+    expect(commentRepository.existsById).toHaveBeenCalledWith(response.commentId);
+    expect(commentRepository.updateLikeCount).toHaveBeenCalledWith(response.commentId, 1);
+    expect(likeRepository.existsCommentLike).toHaveBeenCalledWith(response.userId, response.commentId);
+    expect(likeRepository.saveCommentLike).toHaveBeenCalledWith(response.userId, response.commentId);
+    expect(result.liked).toEqual(true);
+  });
+
+  it('댓글 좋아요 2번 토글 성공', async () => {
+    (commentRepository.existsById as jest.Mock).mockResolvedValue(true);
+    (likeRepository.existsCommentLike as jest.Mock).mockResolvedValue(true);
+    (likeRepository.deleteCommentLike as jest.Mock).mockResolvedValue(response);
+
+    const result = await likeService.toggleCommentLike(response.userId, response.commentId);
+    console.log(JSON.stringify(result, null, 2));
+    expect(commentRepository.existsById).toHaveBeenCalledWith(response.commentId);
+    expect(commentRepository.updateLikeCount).toHaveBeenCalledWith(response.commentId, -1);
+    expect(likeRepository.existsCommentLike).toHaveBeenCalledWith(response.userId, response.commentId);
+    expect(likeRepository.deleteCommentLike).toHaveBeenCalledWith(response.userId, response.commentId);
+    expect(result.liked).toEqual(false);
+  });
+
+  it('댓글 좋아요 토글 실패 (댓글을 찾을 수 없음)', async () => {
+    (commentRepository.existsById as jest.Mock).mockResolvedValue(false);
+
+    await expect(likeService.toggleCommentLike(response.userId, response.commentId)).rejects.toThrow(ExceptionCode.COMMENT_NOT_FOUND.message);
+    expect(commentRepository.existsById).toHaveBeenCalledWith(response.commentId);
   });
 });
