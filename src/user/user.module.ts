7f51aa1e3e07beb6ada828daceaf8baac5332382
Mallangc <cky9001@gmail.com>
2026-01-15 01:24:52 +0900

    feat/user - User Module에 mailer 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,21 @@ import { UserService } from './user.service';
 import {UserRepository} from "./user.repository";
 import {AuthModule} from "../auth/auth.module";
 import {PrismaService} from "../../prisma/prisma.service";
+import {MailerModule} from "@nestjs-modules/mailer";
+import process from "node:process";
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+          user: process.env.EMAIL_USER as string,
+          pass: process.env.EMAIL_PASSWORD as string
+        }
+      }
+    })],
   controllers: [UserController],
   providers: [UserService, UserRepository, PrismaService],
   exports: [UserRepository]
