0f0430097acedcf04e96515199116358abdd1cf6
Mallangc <cky9001@gmail.com>
2026-01-15 01:23:38 +0900

    feat/user - user 인증 로직 추가 - user status에 따라 예외발생하도록 로직 추가 -

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -6,6 +6,7 @@ import {Payload} from "./jwt.payload";
 import {CustomException} from "../../exception/custom.exception";
 import {ExceptionCode} from "../../exception/exception.code";
 import process from "node:process";
+import {UserStatus} from "@prisma/client";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
@@ -13,6 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET_KEY as string,
+      // TODO 배포할 때 false 로 변경
       ignoreExpiration: true,
     });
   }
@@ -21,6 +23,11 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
     const findUser = await this.userRepository.findByEmail(payload.email);
     if (!findUser) {
       throw new CustomException(ExceptionCode.TOKEN_INVALID)
+    }
+    if (findUser.status === UserStatus.DELETED) {
+      throw new CustomException(ExceptionCode.USER_ACCOUNT_DELETED)
+    } else if (findUser.status === UserStatus.BLOCKED) {
+      throw new CustomException(ExceptionCode.USER_ACCOUNT_BLOCKED)
     }
     return findUser;
   }
